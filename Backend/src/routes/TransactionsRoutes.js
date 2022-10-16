414dd511d6336e935e377223e66adbf406e175ff
Paulo Duque <paulo.duque@terra.com.br>
2022-10-16 18:43:29 -0300

    FinSys 1610

--- Backend/src/routes/TransactionsRoutes.js
+++ Backend/src/routes/TransactionsRoutes.js
@@ -1,18 +1,18 @@
 import Router from "express";
 
-const usersRouters = Router();
+const transactionsRoutes = Router();
 
 import { createUserController } from "../modules/users/usecases/CreateUser/index.js";
 import { listUsersController } from "../modules/users/usecases/ListUsers/index.js";
 
 // Rota de criação de transações
-usersRouters.post("/transactions", (request, response) => {
+transactionsRoutes.post("/transactions", (request, response) => {
   return createUserController.handle(request, response);
 });
 
 // Rota de listagem de transações
-usersRouters.get("/users", (request, response) => {
+transactionsRoutes.get("/transactions", (request, response) => {
   return listUsersController.handle(request, response);
 });
 
-export default usersRouters;
+export default transactionsRoutes;
